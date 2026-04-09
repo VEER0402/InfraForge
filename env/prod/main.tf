6a190ca2fd79dfc0d3a204184482ba6e6fd5466f
Veerendra Dwivedi <veerendrad1999@gmail.com>
2026-04-09 16:10:04 +0530

    added env-based deployment

---
+++ env/prod/main.tf
@@ -0,0 +1,28 @@
+terraform {
+ backend "s3" {
+  bucket         = "infraforge-tf-state-veerendra"
+  key            = "prod/terraform.tfstate"
+  region         = "us-east-1"
+  dynamodb_table = "terraform-locks"
+}
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source = "../../modules/vpc"
+}
+
+module "ec2" {
+  source         = "../../modules/ec2"
+  subnet_id      = module.vpc.subnet_id
+  instance_type  = var.instance_type
+  sg_id          = module.sg.sg_id
+}
+
+module "sg" {
+  source = "../../modules/security-group"
+  vpc_id = module.vpc.vpc_id
+}
