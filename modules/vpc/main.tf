a8acf366159bb2cb5b61c0b9237943906d01399d
Veerendra Dwivedi <veerendrad1999@gmail.com>
2026-04-09 07:51:46 +0530

    fixed az - issue

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,8 +7,9 @@ resource "aws_vpc" "main_vpc" {
 }
 
 resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "InfraForge-Public-Subnet"
